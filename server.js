cebc2abcf6c9c876dd9a9be85822905d254d29f6
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-11-08 12:56:36 -0800

    added, addEmployee section

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const connection = mysql.createConnection(
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
 // First prompt the user with an array of questions to get started
-const promptUser = () => {
+//Once user has selected, run the function for the selected option
+const promptUserMenu = () => {
     inquirer.prompt([
-
         {
             type: "list",
             name: "menu",
@@ -35,7 +35,7 @@ const promptUser = () => {
             ]
         }
     ])
-    
+
     .then((data) => {
         switch (data.menu) {
             case "View all departments":
@@ -74,3 +74,119 @@ const promptUser = () => {
 }
 
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+// VIEW SECTION
+// VIEW ALL DEPARTMENTS
+const viewAllDepartments = () => {
+    connection.query("SELECT * FROM department", function (err, result) {
+        console.log("\n");
+        console.table(result);
+        promptUserMenu();
+    })
+}
+
+//VIEW ALL ROLES
+const viewAllRoles = () => {
+    connection.query("SELECT * FROM roles", function (err, result) {
+        console.log("\n");
+        console.table(result);
+        promptUserMenu();
+    })
+}
+
+// VIEW ALL EMPLOYEES 
+const viewAllEmployees = () => {
+    connection.query("SELECT * FROM employees", function (err, result) {
+        console.log("\n");
+        console.table(result);
+        promptUserMenu();
+    })
+}
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+// ADDING SECTION
+// ADD A DEPARTMENT
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "newDepName",
+            message: "What is the name of the new department? "
+        }
+    ])
+    .then((data) => {
+        connection.query("INSERT INTO department (name) VALUES (?)", data.name, (err, result) => {
+            console.log("\nNew Department Created! See new Department below:");
+            viewAllDepartments();
+            promptUserMenu()
+        })
+    })
+} 
+
+// ADD EMPLOYEE 
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "first_name",
+            message: "Enter employee first name",
+        },
+        {
+            type: "input",
+            name: "last_name",
+            message: "Enter employee last name",
+        }
+    ])
+    .then(data => {
+        let params = [data.first_name, data.last_name];
+        connection.query("SELECT title, id FROM role", (err, result) => {
+            if (err) {
+                throw err;
+            }
+            let roles = result.map(({ title, id }) => ({ name: title, value: id}));
+            inquirer.prompt([
+                {
+                    type: "list",
+                    name: "role",
+                    message: "Choose an employee role.",
+                    choices: roles
+                }
+            ])
+            .then(data => {
+                let role = data.role;
+                params.push(role)
+                connection.query("SELECT first_name, last_name, id FROM employees WHERE manager_id IS NULL", (err, result) => {
+                    if (err) {
+                        throw err;
+                    }
+                    let managers = result.map(({ first_name, last_name, id }) => ({ name: `${first_name} ${last_name}`, value: id}));
+                    managers.push({ name: "No Manager", value: null});
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "manager",
+                            message: "Choose the employees manager.",
+                            choices: managers
+                        }
+                    ])
+                    .then(data => {
+                        let manager = data.manager;
+                        params.push(manager);
+                        connection.query("INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)", params, (err) => {
+                            if (err) {
+                                throw err;
+                            }
+                            console.log("New Employee Added!");
+                            promptUserMenu();
+                        })
+                    })
+                })
+            })
+        })
+    })
+};
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+
+//UPDATE SECTION
