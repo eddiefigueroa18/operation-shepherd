7d290d3d5f88ba9e6fb6a4f4c792ae5add471e57
eddiefigueroa18 <eddiefigueroa18@gmail.com>
2022-11-08 09:32:21 -0800

    added tables to schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS management_db;
+CREATE DATABASE management_db;
+
+USE management_db;
+
+-- +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ --
+
+-- 3 Tables: Department, Roles & Employees -- 
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL 
+);
+
+CREATE TABLE roles (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    -- (CONSTRAINT fk_department)? 
+);
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL, 
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+);
